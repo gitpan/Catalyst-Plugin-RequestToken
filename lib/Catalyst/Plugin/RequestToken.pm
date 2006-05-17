b6202c2bf791d8b4690cd6757378a19642f2cd5d
Hideo Kimura <hide@cpan.org>
2006-05-17 13:39:18 +0000

    Import of HIDE/Catalyst-Plugin-RequestToken-0.04 from CPAN.
    gitpan-cpan-distribution: Catalyst-Plugin-RequestToken
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         HIDE/Catalyst-Plugin-RequestToken-0.04.tar.gz
    gitpan-cpan-author:       HIDE
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Plugin/RequestToken.pm
+++ lib/Catalyst/Plugin/RequestToken.pm
@@ -1,35 +1,136 @@
 package Catalyst::Plugin::RequestToken;
 
 use strict;
+use warnings;
+
 use NEXT;
+use Catalyst::Exception ();
+use Digest();
+use overload();
 
-require Data::UUID;
+our $VERSION = '0.04';
 
-our $VERSION = '0.03';
+sub setup {
+    my $c = shift;
+
+    $c->config->{token}->{session_name} ||= 'token';
+    $c->config->{token}->{request_name} ||= 'token';
+	
+    return $c->NEXT::setup(@_);
+}
+
+sub finalize {
+    my $c = shift;
+
+    if ( $c->{_prepare_token} ) {
+        $c->{_prepare_token} = undef;
+        my $name  = $c->config->{token}->{request_name};
+        my $token = $c->create_token;
+        my $body  = $c->response->{body};
+        $body =~ s/(<form\s*.*?>)/$1\n<input type="hidden" name="$name" value="$token">/isg;
+        $c->response->output($body);
+    }
+
+    return $c->NEXT::finalize(@_);
+}
+
+sub prepare_token {
+    my $c = shift;
+
+    $c->{_prepare_token} = 1;
+}
+
+my $counter;
+
+sub create_token {
+    my $c = shift;
+
+    my $digest = $c->_find_digest();
+    my $seed = join("", ++$counter, time, rand, $$, {}, overload::StrVal($c));
+    $digest->add( $seed );
+    my $token = $digest->hexdigest;
+    $c->log->debug("start create token : $token") if $c->debug;
+    $c->session->{$c->config->{token}->{session_name}} = $token;
+    $c->req->params->{ $c->config->{token}->{request_name} } = $token;
+    return $token;
+}
+
+sub remove_token {
+    my $c = shift;
+
+    undef $c->session->{$c->config->{token}->{session_name}};
+}
+
+sub validate_token {
+    my $c = shift;
+
+    my $session = $c->session->{$c->config->{token}->{session_name}};
+    my $request = $c->req->param($c->config->{token}->{request_name});
+
+    return undef if (!defined ($session) && !defined ($request));
+
+    my $res =  $session eq $request ? 1 : undef;
+    if ($c->isa('Catalyst::Plugin::FormValidator::Simple') && !defined($res)) {
+        $c->set_invalid_form($c->config->{token}->{request_name} => 'TOKEN');
+    }
+    return $res; 
+}
+
+# following code is from Catalyst::Plugin::Session
+my $usable;
+
+sub _find_digest () {
+    unless ($usable) {
+        foreach my $alg (qw/SHA-1 SHA-256 MD5/) {
+            if ( eval { Digest->new($alg) } ) {
+                $usable = $alg;
+                last;
+            }
+        }
+        Catalyst::Exception->throw(
+                "Could not find a suitable Digest module. Please install "
+              . "Digest::SHA1, Digest::SHA, or Digest::MD5" )
+          unless $usable;
+    }
+
+    return Digest->new($usable);
+}
+
+1;
+__END__
 
 =head1 NAME
 
 Catalyst::Plugin::RequestToken - Handling transaction token for Catalyst
 
+
 =head1 SYNOPSIS
 
 in your application class:
 
-    use Catalyst qw/Session::FastMmap RequestToken FillForm/;
+    use Catalyst qw/
+        Session
+        Session::State::Cookie
+        Session::Store::FastMmap
+        RequestToken 
+        FillForm
+    /;
+
+in your contoller class:
     
     sub input : Local {
         my ( $self, $c ) = @_;
 
-        $c->stash->{template} = 'input.html';
-        $c->forward('MyApp::V::TT');
+        $c->stash->{template} = 'input.tt';
+        $c->forward($c->view('TT'));
     }
 
     sub confirm : Local {
         my ( $self, $c ) = @_;
 
         $c->create_token;
-        $c->stash->{template} = 'confirm.html';
-        $c->forward('MyApp::V::TT');
+        $c->stash->{template} = 'confirm.tt';
+        $c->forward($c->view('TT'));
         $c->fillform;
     }
 
@@ -44,7 +145,7 @@ in your application class:
         $c->remove_token;
     }
 
-F<root/input.html> TT template:
+F<root/input.tt> TT template:
 
     <html>
     <body>
@@ -54,7 +155,7 @@ F<root/input.html> TT template:
     </body>
     </html>
 
-F<root/confirm.html> TT template:
+F<root/confirm.tt> TT template:
 
     <html>
     <body>
@@ -71,51 +172,39 @@ And you don't have to write '<input type="hidden" name="token"... >' for token i
     sub input : Local {
         my ( $self, $c ) = @_;
 
-        $c->stash->{template} = 'input.html';
+        $c->stash->{template} = 'input.tt';
         $c->prepare_token;
     }
+
+if you loaded L<Catalyst::Plugin::FormValidator::Simple> and fail to validate token, C::P::FormValidator::Simple->set_invalid_form will call automatically in validate_token method (constraint name is 'TOKEN').
+
+    sub complete : Local {
+        my ( $self, $c ) = @_;
+
+        $c->form(
+            name => [qw/NOT_BLANK ASCII/]
+            ...
+        );
+
+        $c->validate_token;
+        
+        my $result = $c->form;
+        
+        if ( $result->has_error) {
+            $c->res->body('Error');
+        } else {
+            $c->res->body('Success');
+        }
+    }
+
 
 =head1 DESCRIPTION
 
 This plugin create, remove and validate transaction token, to be used for enforcing a single request for some transaction, for exapmle, you can prevent duplicate submits.
 
 Note:
-This plugin uses L<Data::UUID> for creating transaction token for each request.  Also this plugin requires a session plugin like L<Catalyst::Plugin::Session::FastMmap> to store server side token.
+REQUIRES a session plugin like L<Catalyst::Plugin::Session> to store server side token.
 
-=head1 EXTENDED METHODS
-
-=over 4
-
-=item setup
-
-You can configure name both of session and request.
-Default name is 'token'.
-
-=cut
-
-sub setup {
-    my $c = shift;
-
-    $c->config->{token}->{session_name} ||= 'token';
-    $c->config->{token}->{request_name} ||= 'token';
-	
-    return $c->NEXT::setup(@_);
-}
-
-sub finalize {
-    my $c = shift;
-    if ( $c->{_prepare_token} ) {
-        $c->{_prepare_token} = undef;
-        my $name  = $c->config->{token}->{request_name};
-        my $token = $c->create_token;
-        my $body  = $c->response->{body};
-        $body =~ s/(<form\s*.*?>)/$1\n<input type="hidden" name="$name" value="$token">/isg;
-        $c->response->output($body);
-    }
-    return $c->NEXT::finalize(@_);
-}
-
-=back
 
 =head1 METHODS
 
@@ -125,81 +214,39 @@ sub finalize {
 
 automatically append token hidden tag to response body.
 
-=cut
-
-sub prepare_token {
-    my $c = shift;
-    $c->{_prepare_token} = 1;
-}
-
 =item create_token
 
-Create new token.
-
-=cut
-
-sub create_token {
-    my $c = shift;
-
-    my $token = new Data::UUID->create_str();
-    $c->log->debug("start create token : $token") if $c->debug;
-    $c->session->{$c->config->{token}->{session_name}} = $token;
-    $c->req->params->{ $c->config->{token}->{request_name} } = $token;
-    return $token;
-}
+Create new token, it uses SHA-1, MD5 or SHA-256, depending on the availibility of these modules.
 
 =item remove_token
 
 Remove token from server side session.
 
-=cut
-
-sub remove_token {
-    my $c = shift;
-
-    undef $c->session->{$c->config->{token}->{session_name}};
-}
-
 =item validate_token
 
 Validate token.
 
-=cut
-
-sub validate_token {
-    my $c = shift;
-
-    my $session = $c->session->{$c->config->{token}->{session_name}};
-    my $request = $c->req->param($c->config->{token}->{request_name});
-
-    return 0 if (!defined ($session) && !defined ($request));
-
-    if ($session eq $request) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
 =back
+
 
 =head1 SEE ALSO
 
-L<Catalyst>, L<Data::UUID>, L<Catalyst::Plugin::Session::FastMmap>
+L<Catalyst>, L<Catalyst::Plugin::Session>, L<Catalyst::Plugin::FormValidator::Simple>
+
 
 =head1 AUTHOR
 
-Hideo Kimura, E<lt>hide@hide-k.netE<gt>
+Hideo Kimura C<< <<hide@hide-k.net>> >>
 
-=head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2005 by Hideo Kimura
+=head1 LICENSE
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
 
 =cut
-
-1;
-__END__
-
